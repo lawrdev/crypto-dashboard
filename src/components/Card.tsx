5f189172cd59e6b0ae39963f5230e395ec1b56af
Unorthodox <lawrdev@gmail.com>
2022-11-14 13:31:34 +0100

    chore: changes interface naming to more unique name

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from "react";
-import { StyledCard } from "./styled/Card.styled";
+import { StyledCard } from "./styled/Cards/Card.styled";
 
-interface ThisProps {
+interface CardProps {
   children: ReactNode;
   variant?: string;
 }
-function Card({ children, variant }: ThisProps) {
+function Card({ children, variant }: CardProps) {
   return <StyledCard variant={variant}>{children}</StyledCard>;
 }
 
