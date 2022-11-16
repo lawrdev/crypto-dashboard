5e6fbd6362c8276d518c7cb5d571df2b18cd3df1
Unorthodox <lawrdev@gmail.com>
2022-11-16 12:57:55 +0100

    deleted

--- src/components/styled/ButtonImage.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-interface StyledButtonImageProps {
-  img: HTMLImageElement | string;
-}
-
-export const StyledButtonImage = styled.div<StyledButtonImageProps>`
-  background-color: "white";
-`;
