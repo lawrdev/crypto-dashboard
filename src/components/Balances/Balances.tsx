351ed83e745c500d97e400a011234fb65862d903
Unorthodox <lawrdev@gmail.com>
2022-12-06 15:00:54 +0100

    feat: adds balances component

---
+++ src/components/Balances/Balances.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  StyledCard,
+  StyledCardHeader,
+  StyledCardTitle,
+} from "../shared/Card/Card.styled";
+import {
+  StyledCardBalances,
+  StyledCardBalancesCurrency,
+  StyledCardBalancesBody,
+} from "./CardBalances.styled";
+import Img from "../assets/img/balances_image.png";
+import Button from "../shared/Buttons/Button";
+import SvgIconLoader from "../shared/SvgIconLoader/SvgIconLoader";
+
+interface BalancesProps {
+  data?: { value: number };
+}
+
+const balanceData: BalancesProps = {
+  data: { value: 9784.79 },
+};
+function Balances(props: BalancesProps) {
+  return (
+    <StyledCard $variant="default">
+      <StyledCardBalances>
+        <StyledCardHeader>
+          <StyledCardTitle>Balances</StyledCardTitle>
+
+          <Button color="secondary" icon="plus" />
+        </StyledCardHeader>
+
+        <StyledCardBalancesCurrency>
+          <div>
+            <SvgIconLoader iconName="dollar_sign" />
+            Dollar
+          </div>
+          <p>{balanceData.data?.value}</p>
+        </StyledCardBalancesCurrency>
+      </StyledCardBalances>
+
+      <StyledCardBalancesBody>
+        <img alt="credit card" src={Img} />
+      </StyledCardBalancesBody>
+    </StyledCard>
+  );
+}
+
+export default Balances;
