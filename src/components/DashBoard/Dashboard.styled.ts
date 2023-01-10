8a9ce202147634cc1357d13906db505eb223480d
Unorthodox <lawrdev@gmail.com>
2023-01-10 10:39:52 +0100

    style: creates wrapper

--- src/components/DashBoard/Dashboard.styled.ts
+++ src/components/DashBoard/Dashboard.styled.ts
@@ -1,11 +1,22 @@
 import styled from "styled-components";
 
-export const StyledWrapper = styled.div``;
+export const StyledWrapper = styled.div`
+  padding-inline-end: 12px;
+`;
 
 export const StyledCoinWrapper = styled.div`
+  padding: 20px 0px;
   display: flex;
   flex-direction: column;
 
-  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+  @media (min-width: ${({ theme }) => theme.sc.breakpoints.md}) {
+    flex-direction: row;
+    flex-wrap: nowrap;
+    gap: 20px;
+    justify-content: space-between;
   }
+`;
+
+export const StyledMarketandBalance = styled.div`
+  margin: 20px 0px;
 `;
