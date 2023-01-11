cead971677270f5e950d2c274c88336c8069d2a0
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-01-11 15:57:01 +0000

    feat: adds dashboard

--- src/components/DashBoard/DashBoardLayout.tsx
+++ src/components/DashBoard/DashBoardLayout.tsx
@@ -1,8 +1,77 @@
-import React from "react";
-import { StyledWrapper } from "./Dashboard.styled";
+import CoinCard from "../shared/Card/CoinCard/CoinCard";
+import {
+  StyledCoinWrapper,
+  StyledMarketandBalance,
+  StyledWrapper,
+} from "./Dashboard.styled";
+import Btc_image from "../../assets/img/coinBTC.png";
+import Ltc_image from "../../assets/img/coinLTC.png";
+import Etm_image from "../../assets/img/coinETM.png";
+import Bnb_image from "../../assets/img/coinBNB.png";
+import MarketOverview from "../MarketOverview/MarketOverview";
+
+interface DataProps {
+  image: string;
+  gain: boolean;
+  coin: string;
+  value: string;
+  valueAvg: string;
+}
+const data: DataProps[] = [
+  {
+    image: Btc_image,
+    gain: true,
+    coin: "BTC",
+    value: "9784.79",
+    valueAvg: "7.2",
+  },
+  {
+    image: Ltc_image,
+    gain: false,
+    coin: "LTC",
+    value: "8741.19",
+    valueAvg: "5.2",
+  },
+  {
+    image: Etm_image,
+    gain: true,
+    coin: "ETM",
+    value: "4567.16",
+    valueAvg: "6.5",
+  },
+  {
+    image: Bnb_image,
+    gain: true,
+    coin: "BNB",
+    value: "6547.79",
+    valueAvg: "9.5",
+  },
+];
 
 function DashBoardLayout() {
-  return <StyledWrapper></StyledWrapper>;
+  return (
+    <StyledWrapper>
+      <StyledCoinWrapper>
+        {/* IMPLEMENT SWIPER JS */}
+
+        {data.map((item, index: number) => (
+          <CoinCard
+            key={index}
+            image={item.image}
+            gain={item.gain}
+            coin={item.coin}
+            value={item.value}
+            valueAvg={item.valueAvg}
+          />
+        ))}
+        {/* reinvent card to accept title */}
+      </StyledCoinWrapper>
+
+      <StyledMarketandBalance>
+        <MarketOverview />
+      </StyledMarketandBalance>
+    </StyledWrapper>
+  );
 }
 
 export default DashBoardLayout;
