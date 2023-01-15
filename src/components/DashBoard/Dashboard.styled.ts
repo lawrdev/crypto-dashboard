021abc1f847251af27b7c616fa20d290ad5da5dc
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-01-15 13:00:00 +0100

    style: adds grid layout

--- src/components/DashBoard/Dashboard.styled.ts
+++ src/components/DashBoard/Dashboard.styled.ts
@@ -4,15 +4,25 @@ export const StyledWrapper = styled.div`
   padding-inline-end: 12px;
 `;
 
+export const StyledGrid = styled.div`
+  display: grid;
+
+  grid-template-columns: repeat(4, 1fr);
+
+  grid-gap: 32px;
+`;
+
 export const StyledCoinWrapper = styled.div`
-  padding: 20px 0px;
+  /* padding: 20px 0px; */
   display: flex;
-  flex-direction: column;
+  /* flex-direction: column; */
+
+  grid-area: 1 / 1 / 2 / -1;
 
   @media (min-width: ${({ theme }) => theme.sc.breakpoints.md}) {
     flex-direction: row;
     flex-wrap: nowrap;
-    gap: 20px;
+    gap: 16px;
     justify-content: space-between;
   }
 `;
